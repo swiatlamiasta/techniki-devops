ba1735a41fe3d7e9758a172e55e8105a8b097478
swiatlamiasta <artgolebiewski@gmail.com>
2020-05-24 11:39:42 +0200

    kubernetes backend cont.

--- kubernetes/mybackend/index.js
+++ kubernetes/mybackend/index.js
@@ -11,6 +11,13 @@ app.get('/', (req, resp)=>{
     resp.send(`${appId} hello from my backend`);
 });
 
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    retry_strategy: () => 1000
+});
+
 app.listen(port, err =>{
     console.log(`Listening on port ${port}`);
 })
